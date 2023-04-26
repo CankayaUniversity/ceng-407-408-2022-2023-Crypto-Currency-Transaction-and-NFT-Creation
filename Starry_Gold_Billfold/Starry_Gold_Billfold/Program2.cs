0d157db6b803cc8f451b4edb25d4831ee8d5542a
İDİL TAŞDAN <111504498+Idiltsdn@users.noreply.github.com>
2023-04-26 23:10:27 +0300

    Transaction-Blockchain

---
+++ Starry_Gold_Billfold/Starry_Gold_Billfold/Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Client;
+using ConsoleApp3;
+using DB;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            // Initilize db
+            DbAccess.Initialize();
+
+            /**
+             * remove all record in all table
+             * uncomment this if you want
+            **/
+            // DbAccess.ClearDB();
+
+            // Make blockchain
+            _ = new Blockchain();
+
+            // show menu
+            Menu.DisplayMenu();
+        }
+    }
+
+
+
+}
