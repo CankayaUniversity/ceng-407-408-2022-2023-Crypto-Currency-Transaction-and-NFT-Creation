e14853d579a11e0b226020df608863164ac590d6
İDİL TAŞDAN <111504498+Idiltsdn@users.noreply.github.com>
2023-04-26 23:07:20 +0300

    Add files via upload

---
+++ Starry_Gold_Billfold/Starry_Gold_Billfold/Grpc/Protos/Bchain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Grpc.Protos
+{
+    internal class Bchain
+    {
+    }
+}
