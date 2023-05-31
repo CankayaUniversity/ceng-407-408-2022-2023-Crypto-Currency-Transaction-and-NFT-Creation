5837a17635a0777c3af077d74fb8d3959234e388
Eylül Aktuy <aktuyeylul99@hotmail.com>
2023-05-31 15:58:36 +0300

    ERC721 NFT ID creation

--- .vscode SGB wallet/NFT/contracts/MyNFT.sol
+++ .vscode SGB wallet/NFT/contracts/MyNFT.sol
@@ -1,3 +1,30 @@
 // SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFT", "MNF") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        _tokenIds.increment();
+
+        return newItemId;
+    }
+}
