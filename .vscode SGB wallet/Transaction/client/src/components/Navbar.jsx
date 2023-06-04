fdd2591526d9705695ec67af2b104c76a468d78e
Eylül Aktuy <aktuyeylul99@hotmail.com>
2023-06-04 15:36:59 +0300

    navbar re-design

--- .vscode SGB wallet/Transaction/client/src/components/Navbar.jsx
+++ .vscode SGB wallet/Transaction/client/src/components/Navbar.jsx
@@ -17,13 +17,12 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
-        ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
-      </ul>
+  <li>
+    <a href="http://localhost:3000/" className=" py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-black">
+      STARRY GOLD BILLFOLD
+    </a>
+  </li>
+</ul>
       <div className="flex relative">
         {!toggleMenu && (
           <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
