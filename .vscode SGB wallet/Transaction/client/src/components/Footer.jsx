698c0d72668ea21232d4447a1a514cf84c9af317
Eylül Aktuy <aktuyeylul99@hotmail.com>
2023-06-04 15:15:28 +0300

    footer re-design

--- .vscode SGB wallet/Transaction/client/src/components/Footer.jsx
+++ .vscode SGB wallet/Transaction/client/src/components/Footer.jsx
@@ -8,12 +8,6 @@ const Footer = () => (
       <div className="flex flex-[0.5] justify-center items-center">
         <img src={logo} alt="logo" className="w-32" />
       </div>
-      <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Tutorials</p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">Wallets</p>
-      </div>
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
