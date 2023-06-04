0f45ec01933e43bf8d65d394f8190fb81949bee6
Buse Çinkaya <80062482+busecinkaya@users.noreply.github.com>
2023-06-04 20:12:26 +0300

    main design

--- .vscode SGB wallet/frontend/client/src/components/Main/index.jsx
+++ .vscode SGB wallet/frontend/client/src/components/Main/index.jsx
@@ -4,6 +4,7 @@ import logo from './images/logo.png'
 import inff from './images/inff.png'
 import logolar from './images/logolar.jpg'
 
+
 const Main = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -17,9 +18,17 @@ const Main = () => {
       
       <p className={styles.baslik} style={{ position: 'absolute', bottom: '0',left: '50%', transform: 'translateX(-50%)',top: '0', color: 'white', fontWeight: 'bold' }}> <h1>STARRY GOLD BILLFOLD</h1> </p>
        
-          
         <div className={styles.logo_container}>
           <img src={inff} alt="inff" className={styles.logo}  style={{ position: 'absolute', bottom: '0',left: '50%', transform: 'translateX(-50%)',top: '0', color: 'white', fontWeight: 'bold' }}></img>
+          <p className={styles.logo_text}  style={{ position: 'absolute', bottom: '150px',left: '50%', transform: 'translateX(-50%)',top: '-50', color: 'white', fontWeight: 'bold' }}>
+          <h3 >Welcome to Starry Gold Billfold, your newest and most secure crypto wallet!</h3>
+
+          A innovative wallet software called Starry Gold Billfold enables customers to safely handle their digital valuables.
+          This platform's user-friendly layout is ideal for both beginners and expert users, making it possible for anyone to easily manage and trade crypto assets.
+          Become a member of Starry Gold Billfold right away to begin managing your digital possessions safely.
+          Choose us for a convenient wallet experience powered by ultra-modern technology.
+        </p>
+
         </div>
        
         
