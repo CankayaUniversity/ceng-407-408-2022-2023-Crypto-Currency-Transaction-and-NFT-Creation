f5e860147f9bba4940f8a9a5268669cbfd01713e
Eylül Aktuy <aktuyeylul99@hotmail.com>
2023-06-07 17:43:44 +0300

    constants - abi & contract address

---
+++ .vscode SGB wallet/Transaction/client/src/utils/constants.js
@@ -0,0 +1,8 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xd181A1F4BD75298d6bD55BBe56e517b4E3fcf224";
+export const infTokenAddress = "0x6E788679F6a5bF57aB90EcB2B6c0a2fBd4A0864e"; //token contract address
+
+
+
